0bf63910e7ffbfc513f3847e2847f5f73d7ed12a
Simon Robinson <im@simon.ws>
2021-10-08 15:32:42 +0100

    First Commit

---
+++ get.php
@@ -0,0 +1,44 @@
+<?php
+ob_start();
+
+if(!isset($_REQUEST['paylink_url'])){
+  $response = [
+    "brf_id" => null,
+    "success" => false
+  ];
+} else {
+  $context = stream_context_create(
+      array(
+          'http' => array(
+              'follow_location' => false
+          )
+      )
+  );
+
+  $html = file_get_contents($_REQUEST['paylink_url'], false, $context);
+  if($content === FALSE){
+    $response = [
+      "brf_id" => null,
+      "success" => false
+    ];
+  } else {
+    $str = substr($http_response_header[2], strpos($http_response_header[2], "id="));
+    $brf_id = substr($str,3);
+
+    if (strpos($brf_id, "BRF") === 0) {
+      $response = [
+        "brf_id" => $brf_id,
+        "success" => true
+      ];
+    } else {
+      $response = [
+        "brf_id" => null,
+        "success" => false
+      ];
+    }
+  }
+}
+
+header('Content-Type: application/json');
+echo json_encode($response);
+?>
---
+++ script.js
@@ -0,0 +1,60 @@
+//Insert the GoCardless Dropin Script
+document.write('<script src="https://pay.gocardless.com/billing/static/dropin/v2/initialise.js"></script>');
+
+var gocardlessHandler = function() {
+  const url = this.getAttribute('data-url');
+  const reqUrl = 'https://simon.ws/gocardless/dev/get.php?paylink_url=' + url;
+  const env = this.getAttribute('data-environment');
+
+  if(isValidHttpUrl(this.getAttribute('data-url')) === false){
+    throw new Error("URL is not properly configured");
+  }
+
+  if (!env.includes("sandbox","production")) {
+    throw new Error("Environment not set properly. Value must be \'sandbox\' or \'production\'.");
+  }
+
+  var req = new XMLHttpRequest();
+  req.responseType = 'json';
+  req.open('GET', reqUrl, true);
+  req.onload  = function() {
+
+     const handler = GoCardlessDropin.create({
+       billingRequestFlowID: req.response.brf_id,
+       environment: env,
+
+       onSuccess: (billingRequest, billingRequestFlow) => {
+         console.log(billingRequest);
+         console.log(billingRequestFlow);
+       },
+
+       onExit: (error, metadata) => {
+         console.error(error);
+         console.error(metadata);
+       },
+     });
+     handler.open();
+
+  };
+  req.send(null);
+};
+
+//Set up button click handler
+document.addEventListener("DOMContentLoaded", function() {
+  document
+    .getElementById("gocardless-btn")
+    .addEventListener("click", gocardlessHandler);
+});
+
+//Functions
+function isValidHttpUrl(string) {
+  let url;
+
+  try {
+    url = new URL(string);
+  } catch (_) {
+    return false;
+  }
+
+  return url.protocol === "http:" || url.protocol === "https:";
+}
